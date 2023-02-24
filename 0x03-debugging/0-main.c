ae8338d928dba71908c737a2833a785b8d9c08ec
FredSoftwareAgency <fredngarick@gmail.com>
2023-02-24 20:23:32 +0300

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
+ * main -  tests functions if integer is positive or negative
  * Return: 0
  */
 
@@ -14,3 +14,4 @@ int main(void)
 
 	return (0);
 }
+
